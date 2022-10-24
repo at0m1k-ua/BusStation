3f34b98b6a08af0001eb8534966e9c518877c9a3
JPlay42 <kuruch2004@gmail.com>
2022-10-24 23:41:00 +0300

    Initial commit

---
+++ src/main/java/com/tv12/busstation/BusStationApplication.java
@@ -0,0 +1,13 @@
+package com.tv12.busstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusStationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusStationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tv12/busstation/BusStationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tv12.busstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusStationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
