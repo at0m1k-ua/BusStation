3dd94f8c9f73a49a7d9507c5c3996e19c541be99
clientforgit <clientforgit@gmail.com>
2023-01-03 04:37:22 +0200

    some-how added JourneysRepository

---
+++ src/main/java/com/tv12/busstation/repositories/JorneyStopsRepository.java
@@ -0,0 +1,7 @@
+package com.tv12.busstation.repositories;
+
+import org.springframework.data.repository.Repository;
+import com.tv12.busstation.entities.JourneyStops;
+
+public interface JorneyStopsRepository extends Repository<JourneyStops, Integer> {
+}
