4d7eac2ec875dd65a8bf0a830c2a3fc8eb5e2fc4
JPlay42 <kuruch2004@gmail.com>
2023-01-06 02:45:03 +0200

    remove unused method

--- src/main/java/com/tv12/busstation/repositories/CitiesRepository.java
+++ src/main/java/com/tv12/busstation/repositories/CitiesRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 @Repository
 public interface CitiesRepository extends JpaRepository<City, Integer> {
-    boolean existsByName(String name);
     List<City> findAllByOrderByNameAsc();
 }
